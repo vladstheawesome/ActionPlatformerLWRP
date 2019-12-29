b8379bfcfb9c9fed68c1b46f320ced8c8af35e79
vladstheawesome <vmabhena@gmail.com>
2019-12-29 01:40:14 +0000

    Combo Attack | Smash Movements

---
+++ Assets/Scripts/Control/States/GravityPull.cs
@@ -0,0 +1,30 @@
+﻿using ActionPlatformer.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActionPlatformer.Control
+{
+    [CreateAssetMenu(fileName = "New State", menuName = "ActionPlatformer/AbilityData/GravityPull")]
+    public class GravityPull : StateData
+    {
+        public AnimationCurve Gravity;
+
+        public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            
+        }
+
+        public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            CharacterControl control = characterState.GetCharacterControl(animator);
+            control.GravityMultiplier = Gravity.Evaluate(stateInfo.normalizedTime);            
+        }
+
+        public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            CharacterControl control = characterState.GetCharacterControl(animator);
+            control.GravityMultiplier = 0f;
+        }
+    }
+}
