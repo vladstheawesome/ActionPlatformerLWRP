504f34d2da1dde1c0ed0708fc7781334b213657c
vladstheawesome <vmabhena@gmail.com>
2020-01-02 01:32:27 +0000

    Character Select | Part 3

---
+++ Assets/Scripts/Control/States/TurnOnRootMotion.cs
@@ -0,0 +1,28 @@
+﻿using ActionPlatformer.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActionPlatformer.Control
+{
+    [CreateAssetMenu(fileName = "New State", menuName = "ActionPlatformer/AbilityData/TurnOnRootMotion")]
+    public class TurnOnRootMotion : StateData
+    {
+        public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            CharacterControl control = characterState.GetCharacterControl(animator);
+            control.SkinnedMeshAnimator.applyRootMotion = true;
+        }
+
+        public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+           
+        }
+
+        public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            CharacterControl control = characterState.GetCharacterControl(animator);
+            control.SkinnedMeshAnimator.applyRootMotion = false;
+        }
+    }
+}
