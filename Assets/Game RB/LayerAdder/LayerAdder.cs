e1fc95c1b3680b320814ffa298b241e4fd2dbec8
vladstheawesome <vmabhena@gmail.com>
2020-01-12 00:44:57 +0000

    Adding Layers via Script

---
+++ Assets/Game RB/LayerAdder/LayerAdder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActionPlatformer
+{
+    public enum RB_Layers
+    {
+        PostProcessing,
+        CHARACTER,
+        BOT,
+        GROUND,
+    }
+
+    public class LayerAdder : MonoBehaviour
+    {
+
+    }
+}
