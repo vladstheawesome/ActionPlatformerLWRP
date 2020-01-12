67c432b71db6eb979cccf77fe3f7a4b883d61f2b
vladstheawesome <vmabhena@gmail.com>
2020-01-12 02:45:28 +0000

    Using the NavMeshAgent | Part 3 & 4

--- Assets/Scripts/Core/PathFindingAgent/PathFindingAgent.cs
+++ Assets/Scripts/Core/PathFindingAgent/PathFindingAgent.cs
@@ -11,6 +11,13 @@ namespace ActionPlatformer.Core
         public GameObject target;
         NavMeshAgent navMeshAgent;
 
+        public Vector3 StartPosition;
+        public Vector3 EndPosition;
+        Coroutine Move;
+
+        public GameObject StartSphere;
+        public GameObject EndSphere;
+
         private void Awake()
         {
             navMeshAgent = GetComponent<NavMeshAgent>();
@@ -18,12 +25,54 @@ namespace ActionPlatformer.Core
 
         public void GoToTarget()
         {
+            StartSphere.transform.parent = null;
+            EndSphere.transform.parent = null;
+
+            navMeshAgent.isStopped = false;
+
             if (TargetPlayableCharacter)
             {
                 target = CharacterManager.Instance.GetPlayableCharacter().gameObject;
             }
 
             navMeshAgent.SetDestination(target.transform.position);
+
+            if (Move != null)
+            {
+                StopCoroutine(Move);
+            }
+
+            Move = StartCoroutine(_Move());
+        }
+
+        IEnumerator _Move()
+        {
+            while(true)
+            {
+                if (navMeshAgent.isOnOffMeshLink)
+                {
+                    StartPosition = transform.position;
+                    StartSphere.transform.position = transform.position;
+                    navMeshAgent.CompleteOffMeshLink();
+
+                    yield return new WaitForEndOfFrame();
+                    EndPosition = transform.position;
+                    EndSphere.transform.position = transform.position;
+                    navMeshAgent.isStopped = true;
+                    yield break;
+                }
+
+                Vector3 dist = transform.position - navMeshAgent.destination;
+                if (Vector3.SqrMagnitude(dist) < 0.5f)
+                {
+                    StartPosition = transform.position;
+                    EndPosition = transform.position;
+                    navMeshAgent.isStopped = true;
+                    yield break;
+                }
+
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
